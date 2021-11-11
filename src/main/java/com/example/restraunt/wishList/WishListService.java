dca2837cf9be3ea8f7a4d8f899e4481adf20071a
lkjh8sda <lkjh8sda@naver.com>
2021-11-11 22:25:53 +0900

    ~~

--- src/main/java/com/example/restraunt/wishList/WishListService.java
+++ src/main/java/com/example/restraunt/wishList/WishListService.java
@@ -1,6 +1,8 @@
 package com.example.restraunt.wishList;
 
 import com.example.restraunt.naver.NaverClient;
+import com.example.restraunt.naver.dto.SearchImageReq;
+import com.example.restraunt.naver.dto.SearchLocalReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,28 @@ public class WishListService {
     private final NaverClient naverClient;
 
     public void search(String query){
+
         //지역검색
+        var searchLocalReq = new SearchLocalReq();
+        searchLocalReq.setQuery(query);
 
-        //이미지 검색
+        var searchLocalRes = naverClient.localSearch(searchLocalReq);
 
-        //결과 리턴
+        if(searchLocalRes.getTotal() > 0){
+            var item = searchLocalRes.getItems().stream().findFirst().get();
+
+            var imageQuery = item.getTitle().replaceAll("<[^>]*>","");
+            var searchImageReq = new SearchImageReq();
+            searchImageReq.setQuery(imageQuery);
+            //이미지 검색
+            var searchImageRes = naverClient.imageSearch(searchImageReq);
+
+            if(searchImageRes.getTotal() > 0){
+                //결과 리턴
+                searchImageRes.getItems().stream().findFirst().get();
+            }
+        }
+
     }
 
 }
